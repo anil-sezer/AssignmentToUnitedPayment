d5ba30afeb0a5f80ab290ed390fc221452b09080
anil <msnanil@hotmail.com>
2023-06-09 15:38:59 +0300

    feat: Added auto service registration

--- src/UPay.Web.Api/Program.cs
+++ src/UPay.Web.Api/Program.cs
@@ -1,14 +1,16 @@
 using Serilog;
 using Serilog.Events;
+using UPay.Application.Boilerplate;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient();
+
+builder.Services.ConfigureApplicationService();
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
@@ -19,7 +21,6 @@ Log.Logger = new LoggerConfiguration()
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
